62606108ae2411b4c0941df22a91bdf7399d9199
Allan Ebdrup <allan@878.dk>
2016-06-06 11:37:25 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var helmet = require('helmet');
 var ONE_YEAR = 31536000000;
 module.exports = function () {
-	return function (request, response, next) {
+	return function (req, res, next) {
 		if (request.headers['x-forwarded-proto'] === 'http') {
 			response.redirect(301, 'https://' + request.headers.host + request.url);
 			return;
@@ -10,7 +10,7 @@ module.exports = function () {
 			maxAge: ONE_YEAR,
 			includeSubdomains: true,
 			force: true
-		})(request, response, function(req, res, next){
+		})(req, res, function(){
 			helmet.hidePoweredBy()(req, res, next);
 		});
 	};
